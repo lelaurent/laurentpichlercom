20c8463898fb5424e8f72e39215e8d11935f710a
Robin Gruenke <eimfach@me.com>
2019-10-29 14:35:30 +0100

    project configuration and documentation

---
+++ tailwind.config.js
@@ -0,0 +1,10 @@
+// Tailwind config file for configuring generated css features. See https://tailwindcss.com/docs
+module.exports = {
+    theme: {
+      extend: {}
+    },
+    variants: {
+      backgroundColor: ['responsive', 'hover', 'focus', 'active']
+    },
+    plugins: []
+  }
